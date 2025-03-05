5c15ec3711b3210435b64fbe8ae4335bbbbf2c6b
Eugene Wang <8755753+soukoku@users.noreply.github.com>
2025-03-05 08:54:45 -0500

    Check for empty path in TryLoadCustomDSM.

--- src/NTwain/DSM/DsmLoader.cs
+++ src/NTwain/DSM/DsmLoader.cs
@@ -11,40 +11,52 @@ using System.Threading.Tasks;
 
 namespace NTwain.DSM
 {
-  /// <summary>
-  /// For demoing loading dsm from custom path in case
-  /// it's not installed on system and don't want to be 
-  /// placed besides the exe.
-  /// </summary>
-  static class DsmLoader
-  {
-    static IntPtr __dllPtr;
-
-    public static bool TryLoadCustomDSM()
+    /// <summary>
+    /// For demoing loading dsm from custom path in case
+    /// it's not installed on system and don't want to be 
+    /// placed besides the exe.
+    /// </summary>
+    static class DsmLoader
     {
-      if (__dllPtr == IntPtr.Zero)
-      {
-        var curFile = Assembly.GetExecutingAssembly().Location;
+        static IntPtr __dllPtr;
 
-        var dll = Path.Combine(
-          Path.GetDirectoryName(curFile)!,
-          $@"runtimes\win-{(TWPlatform.Is32bit ? "x86" : "x64")}\native\TWAINDSM.dll");
-
-        __dllPtr = LoadLibraryW(dll);
-
-        if (__dllPtr != IntPtr.Zero)
+        public static bool TryLoadCustomDSM()
         {
-          Debug.WriteLine("Using our own dsm now :)");
+            if (__dllPtr == IntPtr.Zero)
+            {
+#if NETFRAMEWORK
+                var curFile = Assembly.GetExecutingAssembly().Location;
+                if (string.IsNullOrEmpty(curFile))
+                {
+                    using var proc = Process.GetCurrentProcess();
+                    curFile = proc.MainModule.FileName;
+                }
+                var folder = Path.GetDirectoryName(curFile);
+#else
+                var folder = AppContext.BaseDirectory;
+#endif
+                if (!string.IsNullOrEmpty(folder))
+                {
+                    var dll = Path.Combine(
+                      folder,
+                      $@"runtimes\win-{(TWPlatform.Is32bit ? "x86" : "x64")}\native\TWAINDSM.dll");
+
+                    __dllPtr = LoadLibraryW(dll);
+                }
+
+                if (__dllPtr != IntPtr.Zero)
+                {
+                    Debug.WriteLine("Using our own dsm now :)");
+                }
+                else
+                {
+                    Debug.WriteLine("Will attempt to use default dsm :(");
+                }
+            }
+            return __dllPtr != IntPtr.Zero;
         }
-        else
-        {
-          Debug.WriteLine("Will attempt to use default dsm :(");
-        }
-      }
-      return __dllPtr != IntPtr.Zero;
+
+        [DllImport("kernel32", SetLastError = true)]
+        static extern IntPtr LoadLibraryW([MarshalAs(UnmanagedType.LPWStr)] string lpFileName);
     }
-
-    [DllImport("kernel32", SetLastError = true)]
-    static extern IntPtr LoadLibraryW([MarshalAs(UnmanagedType.LPWStr)] string lpFileName);
-  }
 }
